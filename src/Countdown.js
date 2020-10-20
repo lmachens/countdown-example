6ba076230cd9a7f80da8563bc83b8b8d4601be5b
lmachens <leon.machens@googlemail.com>
2020-10-20 14:53:20 +0200

    Add interval

--- src/Countdown.js
+++ src/Countdown.js
@@ -1,5 +1,11 @@
 export function Countdown() {
   const container = document.createElement("div");
   container.textContent = "100";
+
+  let timeLeft = 100;
+  setInterval(() => {
+    timeLeft--;
+    console.log(timeLeft);
+  }, 1000);
   return container;
 }
