7d422d56f1a90a618d31d7fca499e5edd4f3008b
lmachens <leon.machens@googlemail.com>
2020-10-20 16:46:52 +0200

    Start countdown

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ export function App() {
   const countdown = Countdown();
   function onSubmit(value) {
     countdown.innerText = value;
-    console.log(value);
+    const intervalId = setInterval(() => {
+      if (countdown.innerText === "0") {
+        clearInterval(intervalId);
+      } else {
+        countdown.innerText = countdown.innerText - 1;
+      }
+    }, 1000);
   }
   const countdownForm = CountdownForm(onSubmit);
   const app = document.createElement("div");
--- src/Countdown.js
+++ src/Countdown.js
@@ -1,13 +1,5 @@
 export function Countdown() {
   const container = document.createElement("div");
   container.innerText = "0";
-
-  const intervalId = setInterval(() => {
-    if (container.innerText === "0") {
-      clearInterval(intervalId);
-    } else {
-      container.innerText = container.innerText - 1;
-    }
-  }, 1000);
   return container;
 }
