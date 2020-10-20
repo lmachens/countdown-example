7a0bfab6782d39aeb05d4d4c8a914cb54b9e2335
lmachens <leon.machens@googlemail.com>
2020-10-20 14:50:18 +0200

    Prepare countdown component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./app.css";
+import { Countdown } from "./Countdown";
 
 export function App() {
   const app = document.createElement("div");
   app.className = "app";
+  const countdown = Countdown();
+
   app.innerHTML = "<b>Awesome</b> Webpack Template!";
+  app.append(countdown);
   return app;
 }
---
+++ src/Countdown.js
@@ -0,0 +1,5 @@
+export function Countdown() {
+  const container = document.createElement("div");
+  container.textContent = "100";
+  return container;
+}
