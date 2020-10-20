1206c210021522dbe20d63f5adbaf36977acc191
lmachens <leon.machens@googlemail.com>
2020-10-20 14:54:56 +0200

    Update container text based on timeLeft

--- src/Countdown.js
+++ src/Countdown.js
@@ -1,11 +1,11 @@
 export function Countdown() {
   const container = document.createElement("div");
-  container.textContent = "100";
+  let timeLeft = 200;
+  container.innerText = timeLeft;
 
-  let timeLeft = 100;
   setInterval(() => {
     timeLeft--;
-    console.log(timeLeft);
+    container.innerText = timeLeft;
   }, 1000);
   return container;
 }
